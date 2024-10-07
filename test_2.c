30e31d998fa2c827cc63962053b6634d2b305b2d
LMIKEY <19926691481@163.com>
2024-10-07 20:18:40 +0800

    添加功能：记录收入、记录支出

---
+++ test_2.c
@@ -0,0 +1,83 @@
+#include<stdio.h>
+
+int main() {
+#include<stdio.h>
+#include <stdlib.h>
+#define _CRT_SECURE_NO_WARNINGS 1
+
+	typedef struct amout {
+		float get;
+		float pay;
+	}amout;
+
+	typedef struct node {
+		amout amt;
+		struct node* next;
+	}node;
+
+	node* head = NULL;
+
+	void recordGet() {
+		printf("Please enter the income data to be recorded\n");
+		node* newNode = (node*)malloc(sizeof(node));
+		newNode->next = NULL;
+		if (head = NULL) {
+			head = newNode;
+		}
+		else {
+			newNode->next = head;
+			head = newNode;
+		}
+		float money;
+		printf("Please enter£º");
+		scanf("%f", &money);
+		newNode->amt.get = money;
+		printf("\nPlease enter\n");
+	}
+
+	void recordPay() {
+		printf("Please enter the expenditure data to be recorded\n");
+		node* newNode = (node*)malloc(sizeof(node));
+		newNode->next = NULL;
+		if (head = NULL) {
+			head = newNode;
+		}
+		else {
+			newNode->next = head;
+			head = newNode;
+		}
+		float money;
+		printf("Please enter£º");
+		scanf("%f", &money);
+		newNode->amt.pay = money;
+		printf("\nRecord success\n");
+	}
+	int main() {
+		while (1) {
+			printf("Please select the function of recording income or expenses\n");
+			printf("1¡¢Record income\n");
+			printf("2¡¢Record expenses\n");
+			printf("0¡¢quit");
+			printf("Please press the option serial number.\n");
+			char ch = getch();
+			switch (ch) {
+			case '1':
+				recordGet();
+				break;
+			case'2':
+				recordPay();
+				break;
+			case'0':
+				printf("successful exit\n");
+				return 0;
+				break;
+			}
+		}
+
+		return 0;
+
+	}
+
+	
+	return;
+}
